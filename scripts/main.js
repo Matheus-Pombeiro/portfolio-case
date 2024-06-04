fbba62d7ebe37fdc0c96929064d7a38ba9c7fdba
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-04 11:16:57 -0300

    feat: comments to explain the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
-import menu from "./menu.js";
+import menu from "./menu.js";   // importa a função menu()
 
 // Recebe os elementos do html
 const menuBtn = document.querySelector("[data-menu-btn]");
 const navMobile = document.querySelector("[data-nav-mobile]");
 
-menuBtn.addEventListener("click", () => console.log("clickou"))
-
-menu(menuBtn, navMobile);
+menu(menuBtn, navMobile);   // Chama a função menu()
