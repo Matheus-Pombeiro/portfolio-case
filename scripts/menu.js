fdd694d4c94601ef8cc3451e2229ef9985db0fe4
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-04 18:31:01 -0300

    fix: navbar display bug

--- scripts/menu.js
+++ scripts/menu.js
@@ -7,4 +7,14 @@ export default function menu(menuBtn, navMobile) {
             ? navMobile.style.display = "none" 
             : navMobile.style.display = "flex";
     });
+
+    // Escuta a mudança no tamanho da tela do dispositivo 
+    window.matchMedia("(min-width: 1280px)").addEventListener("change", (e) => {
+        // Recebe o tamanho da tela sempre que há uma mudança
+        const desktopScreen = e.matches;
+        // Manipula o display do navbar baseado no tamanho da tela, evitando bugs
+        desktopScreen 
+            ? navMobile.style.display = "flex" 
+            : navMobile.style.display = "none";
+    });
 };
